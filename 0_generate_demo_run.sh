2d9816dbe9863c7fe39a5f7ece5465f9e4ca9de1
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2018-01-13 18:55:43 +0100

    Update 0_generate_demo_run.sh

--- 0_generate_demo_run.sh
+++ 0_generate_demo_run.sh
@@ -14,8 +14,8 @@ tmp2=$(mktemp)
 echo "tmp::${tmp}"
 echo "tmp2::${tmp2}"
 
-zcat /scratch/nussbaumer/copy_to_hard_disk/to_upload_VITRO/HDNecplusB1h-1_S2_L002_R1_001.fastq.gz | head -n 10000 > $tmp
-zcat /scratch/nussbaumer/copy_to_hard_disk/to_upload_VITRO/HDNecplusB1h-1_S2_L002_R2_001.fastq.gz | head -n 10000 > $tmp2
+zcat BLA_R1_001.fastq.gz | head -n 10000 > $tmp
+zcat BLA_R2_001.fastq.gz | head -n 10000 > $tmp2
 
 wc -l $tmp
 wc -l $tmp2
@@ -25,18 +25,17 @@ RVS=$tmp2
 
 X=/proj/Neisseria_meningitidis/5.mapping_pipeline/demo_data
 
-HS_INDEX=/scratch/nussbaumer/neisseria_V2/data/HOMO_SAPIENS_INDEX
-MS_INDEX=/scratch/nussbaumer/neisseria_V2/data/MUS_MUSCULUS_INDEX
+HS_INDEX=HOMO_SAPIENS_INDEX
+MS_INDEX=MUS_MUSCULUS_INDEX
 
-NM_REF=/scratch/nussbaumer/neisseria_V2/data/references/GCF_000026965.1_ASM2696v1_genomic.fna
-HS_REF=/scratch/nussbaumer/neisseria_V2/data/references/Homo_sapiens.GRCh38.dna.primary_assembly.fa
-MS_REF=/scratch/nussbaumer/neisseria_V2/data/references/Mus_musculus.GRCm38.dna_rm.primary_assembly.fa
+NM_REF=references/GCF_000026965.1_ASM2696v1_genomic.fna
+HS_REF=references/Homo_sapiens.GRCh38.dna.primary_assembly.fa
+MS_REF=references/Mus_musculus.GRCm38.dna_rm.primary_assembly.fa
 
-NM_GFF=/scratch/nussbaumer/neisseria_V2/data/gff/GCF_000026965.1_ASM2696v1_genomic_V2.gff
-HS_GFF=/scratch/nussbaumer/neisseria_V2/data/gff/Homo_sapiens.GRCh38.87_modified.gff3
-MS_GFF=/scratch/nussbaumer/neisseria_V2/data/gff/Mus_musculus.GRCm38.87_modified.gff3
+NM_GFF=gff/GCF_000026965.1_ASM2696v1_genomic_V2.gff
+HS_GFF=gff/Homo_sapiens.GRCh38.87_modified.gff3
+MS_GFF=gff/Mus_musculus.GRCm38.87_modified.gff3
 
-P=1
 C=1
 
 echo "FWD::$FWD"
